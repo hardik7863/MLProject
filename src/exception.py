89f6a32f1b09a8f26f06f72e75bc64a4a276c82f
hardik7863 <hardikbatwal@gmail.com>
2025-02-19 15:25:24 +0530

    data ingestion completed

--- src/exception.py
+++ src/exception.py
@@ -20,10 +20,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by ZeroError")
-        raise CustomException(e,sys)
+
         
